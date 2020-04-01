061eda0f900e8261c682f185729562379c033a26
jacob <jacobriis86@hotmail.com>
2020-04-01 11:40:59 +0200

    minor fixes

--- src/main/java/Logik/Cabal.java
+++ src/main/java/Logik/Cabal.java
@@ -2,22 +2,33 @@ package Logik;
 
 import java.util.List;
 
+/**
+ * This is the model of the entire cabal
+ */
 public class Cabal implements I_CabalModel{
 
     private List<CardStack> columns;
-    private List<CardStack> AcesPile;
+    private List<CardStack> acesPile;
     private List<CardStack> turnedPile;
     private List<CardStack> cardPile;
     private boolean isCabalSet;
 
+    public Cabal(List<CardStack> columns, List<CardStack> acesPile, List<CardStack> turnedPile, List<CardStack> cardPile, boolean isCabalSet) {
+        this.columns = columns;
+        this.acesPile = acesPile;
+        this.turnedPile = turnedPile;
+        this.cardPile = cardPile;
+        this.isCabalSet = isCabalSet;
+    }
 
+    //---------------------------------------Getters------------------------------------------------------------------------
 
     public List<CardStack> getColumns() {
         return columns;
     }
 
     public List<CardStack> getAcesPile() {
-        return AcesPile;
+        return acesPile;
     }
 
     public List<CardStack> getTurnedPile() {
@@ -32,9 +43,13 @@ public class Cabal implements I_CabalModel{
         return isCabalSet;
     }
 
+//---------------------------------------Setters------------------------------------------------------------------------
+
     public void setCabalSet(boolean cabalSet) {
         isCabalSet = cabalSet;
     }
+
+//---------------------------------------Various methods----------------------------------------------------------------
 
     @Override
     public I_CardModel turnCard() {
