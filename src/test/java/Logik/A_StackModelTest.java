a79d8954c53c05d019dace0c46f3e8e411e9160d
Goju-Ryu <mads1606@gmail.com>
2020-04-03 16:29:37 +0200

    Added test of the concrete method in A_StackModel

---
+++ src/test/java/Logik/A_StackModelTest.java
@@ -0,0 +1,58 @@
+package Logik;
+
+import Logik.error.IllegalMoveException;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class A_StackModelTest {
+
+    @Test
+    void addToStack() {
+        A_StackModel stack = new A_StackModel() {
+            @Override
+            public boolean canMoveTo(A_StackModel stackModel) {
+                return false;
+            }
+
+            @Override
+            public void moveTo(A_StackModel stackModel) throws IllegalMoveException {
+
+            }
+
+            @Override
+            public A_StackModel splitAt(int position) {
+                return null;
+            }
+
+            @Override
+            public I_CardModel getFirst() {
+                return null;
+            }
+
+            @Override
+            public I_CardModel getCardAt(int position) {
+                return null;
+            }
+
+            @Override
+            public I_CardModel getLast() {
+                return null;
+            }
+        };
+        stack.cards = new ArrayList<>();
+
+        stack.addToStack(new ArrayList<I_CardModel>(){{
+                             new Card(E_CardSuit.DIAMONDS, E_CardRank.FIVE);
+                             new Card();
+                             new Card();
+                             new Card(E_CardSuit.CLUBS, E_CardRank.EIGHT);
+                         }}
+        );
+
+        assertNotNull(stack.cards);
+        assertEquals(5, stack.cards.size());
+    }
+}
