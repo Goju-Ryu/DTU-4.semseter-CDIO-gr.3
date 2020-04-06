1f1afb40f9705e589edbf2222d65a994c7bc918a
Goju-Ryu <mads1606@gmail.com>
2020-04-06 22:35:42 +0200

    Added a beforeEach method

--- src/test/java/Logik/CardTest.java
+++ src/test/java/Logik/CardTest.java
@@ -1,10 +1,19 @@
 package Logik;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CardTest {
+    I_CardModel faceUp;
+    I_CardModel faceDown;
+
+    @BeforeEach
+    void beforeEach(){
+        faceUp = new Card(E_CardSuit.DIAMONDS, E_CardRank.FIVE);
+        faceDown = new Card();
+    }
 
     @Test
     void getSuit() {
