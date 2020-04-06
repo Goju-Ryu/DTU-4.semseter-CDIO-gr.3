b2a824102c029c48e855d61f5af39697c058278e
Goju-Ryu <mads1606@gmail.com>
2020-04-06 22:07:54 +0200

    Added tests for the card class

---
+++ src/test/java/Logik/CardTest.java
@@ -0,0 +1,36 @@
+package Logik;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardTest {
+
+    @Test
+    void getSuit() {
+    }
+
+    @Test
+    void getSuitText() {
+    }
+
+    @Test
+    void getRank() {
+    }
+
+    @Test
+    void getRankValue() {
+    }
+
+    @Test
+    void isFacedUp() {
+    }
+
+    @Test
+    void setFacedUp() {
+    }
+
+    @Test
+    void toStringValue() {
+    }
+}
