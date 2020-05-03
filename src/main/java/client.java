944634d9d2164a87e6f027ac2124cbd4b5221b29
Goju-Ryu <mads1606@gmail.com>
2020-05-03 13:37:43 +0200

    added the compiler plugin

--- src/main/java/client.java
+++ src/main/java/client.java
@@ -1,5 +1,12 @@
+import java.util.logging.Logger;
+
 public class client {
+    private static final Logger logger = Logger.getLogger(client.class.getName());
+
+    //private final GreeterGrpc.GreeterBlockingStub blockingStub;
+
     public static void main(String[] args) {
         System.out.println("hello");
+
     }
 }
