993c64233e15bae8171f6f38d3ebc9246a4cd76e
Goju-Ryu <mads1606@gmail.com>
2020-06-04 11:04:21 +0200

    Fixed bug causing the generated grpc python file to not import the other gennerated file correctly

--- src/main/python/proto/protoc.sh
+++ src/main/python/proto/protoc.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # Script for gennerating grpc files in python
 cd "${BASH_SOURCE%/*}"  ||  (echo "ERROR: Could not find \${BASH_SOURCE%/*}" && exit )
-python -m grpc_tools.protoc -I../../proto --python_out=. --grpc_python_out=. ../../proto/grpc.proto
-#      ^choose module to run               ^placement of output
+#  ^go to relevant directory   ^or write an error and exit if failed
+
+python -m grpc_tools.protoc --proto_path=../.. --python_out=.. --grpc_python_out=.. ../../proto/grpc.proto
+#      ^choose module to run  ^homedir of proto   ^1st file out   ^2nd file out      ^proto to compile from
