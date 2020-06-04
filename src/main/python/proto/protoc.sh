6832ca3e3d5092e88e6be8548319b75fc2dfd869
Goju-Ryu <mads1606@gmail.com>
2020-06-04 09:03:58 +0200

    Added support for a fully maven driven genneration of grpc files. protoc.sh is a script that gennerates the grpc python files. It is using relative paths so don't move the script or the proto file without edditing it. in the pom.xml i added a plugin which handles the execution of the script during compile.
    NOTE: The maven plugin seems to be bad at handeling the genneration of files from proto files. use command line with the commandes "mvn clean" and "mvn compile" if the plugin throws errors.

--- src/main/python/proto/protoc.sh
+++ src/main/python/proto/protoc.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # Script for gennerating grpc files in python
+cd "${BASH_SOURCE%/*}"  ||  (echo "ERROR: Could not find \${BASH_SOURCE%/*}" && exit )
 python -m grpc_tools.protoc -I../../proto --python_out=. --grpc_python_out=. ../../proto/grpc.proto
 #      ^choose module to run               ^placement of output
