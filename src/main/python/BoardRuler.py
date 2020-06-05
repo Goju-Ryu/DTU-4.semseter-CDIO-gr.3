igang med fix til random rotation

src/main/python/BoardRuler.py
@@ -2,10 +2,6 @@ import cv2
 import numpy as np
 
 class BoardRuler:
-
-
-
-    #nnybranch!
     def decorateImageRulerLines(self, image):
         height = image.shape[0]
         width = image.shape[1]
@@ -53,28 +49,19 @@ class BoardRuler:
         for i in range (0,7):
             im = cv2.getRectSubPix(image, (98, 33), (1, 1))
 
-
-
     def isolate(self, img):
-
         # Get the Green Color
-        image = cv2.GaussianBlur(img, (11, 11), 0)
-        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        image = cv2.GaussianBlur(img, (5,5), 0)
 
-        # define range of blue color in HSV
-        lower_blue = np.array([60,60,60])
-        upper_blue = np.array([132,230,230])
-        # Threshold the HSV image to get only blue colors
-        mask = cv2.inRange(hsv, lower_blue, upper_blue)
-
-
-
+        mask = self.interactiveMaskWindow(image)
         pts, succes = self.findBoardContour(mask, image)
         if succes:
             p1 = (pts[0][0][0], pts[0][0][1])
             p2 = (pts[1][0][0], pts[1][0][1])
             p3 = (pts[2][0][0], pts[2][0][1])
             p4 = (pts[3][0][0], pts[3][0][1])
+
+            self.SortPoints(p1,p2,p3,p4)
 
             nPts = np.float32([
                 [0, 0],
@@ -87,47 +74,61 @@ class BoardRuler:
             matrix = cv2.getPerspectiveTransform(pts, nPts)
             result = cv2.warpPerspective(img, matrix, (600, 400))
 
-
-            return result , True
+            return result ,mask, True
         else:
-            return None, False
+            return None,mask, False
+
+    def empty(self,a):
+        pass
+
+    def interactiveMaskWindow(self, image):
+
+        """windowName = "BoardMaskEditor"
+        cv2.namedWindow(windowName)
+        cv2.resizeWindow(windowName,600,200)
+
+        cv2.createTrackbar("Hue Min", windowName, 0 , 179, self.empty)
+        cv2.createTrackbar("Sat Min", windowName, 0, 179, self.empty)
+        cv2.createTrackbar("Val Min", windowName, 0, 179, self.empty)
+        """
+
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+        # define range of blue color in HSV
+        lower_blue = np.array([60,60,60])
+        upper_blue = np.array([90,230,230])
+        mask = cv2.inRange(hsv, lower_blue, upper_blue)  # Threshold the HSV image to get only blue colors
+        return mask
 
     def findBoardContour (self, mask,  image):
+        contours, hiarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL ,cv2.CHAIN_APPROX_SIMPLE)
 
-        #cv2.imshow("mask", mask)
-        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL ,cv2.CHAIN_APPROX_SIMPLE)
+        # if there are no Contours
         if len(contours) == 0:
             return [], False
-
-        # sorting contours for size
-        Sorted = sorted(range(len(contours)), key=lambda i: cv2.contourArea(contours[i]), reverse=True)
-
-        for i in range(len(Sorted)):
-
-            # reduces the contour vertices, with strange constants. that somehow works, look at reference.
+        mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+        for i in range(len(contours)):
+            # Simplify into Polygon.
             epsilon = cv2.arcLength(contours[i], True)
             approx = cv2.approxPolyDP(contours[i], 0.04 * epsilon, True)  # now we have a simplified polygon.
 
+            # an array of size 1, to show that Contour.
             appr = []
-            apprIndex = 0
+            appr.append(approx)
+
             if len(approx) == 4:
-                if contours[i].size > 400:
-                    print(contours[i].size)
-                    # drawing contours
-                    appr.append(approx)
-                    cv2.drawContours(mask, contours, i, (100, 100, 100), 10)
-                    cv2.drawContours(image, appr, apprIndex, (144, 247, 155), 10)
-                    apprIndex += 1
+                if contours[i].size > 200:
+                    cv2.drawContours( mask , appr, -1  , (0, 255, 0), 5)
+                    cv2.drawContours( image, appr       , -1  , (144, 247, 155), 10)
             else:
-
-                cv2.drawContours(image, contours, i, (255, 0, 0), 1)
-                opr = []
-                opr.append(approx)
-
-                cv2.drawContours(image, opr, 0, (0, 255, 0), 1)
+                cv2.drawContours(image, contours[i], -1, (255, 0, 0), 1)
+                cv2.drawContours(image, appr, -1, (0, 255, 0), 1)
 
         #cv2.imshow("CONTOUR", image)
         if (len(appr) != 1):
             return None, False
         else:
             return appr[0],True
+
+    def SortPoints(self, p1, p2 , p3 , p4):
+        pass