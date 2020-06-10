abf47fdc3b5109346bcd3cc96d9b503a95287f15
jacob <jacobriis86@hotmail.com>
2020-06-10 08:40:58 +0200

    fixed junit issue

---
+++ src/test/java/model/cabal/BoardTest.java
@@ -0,0 +1,30 @@
+package model.cabal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardTest {
+
+    @org.junit.jupiter.api.Test
+    void turnCard() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTurnedCard() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isStackComplete() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void move() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void canMove() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPile() {
+    }
+}
