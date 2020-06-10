5cffe73518bc0013efa6e20da843ea32cada0a1a
Goju-Ryu <mads1606@gmail.com>
2020-06-10 21:06:12 +0200

    Organised method placement in document

--- src/main/java/model/cabal/Board.java
+++ src/main/java/model/cabal/Board.java
@@ -1,6 +1,6 @@
 package model.cabal;
 
-
+import model.cabal.internals.I_SolitaireStacks;
 import model.cabal.internals.card.I_CardModel;
 import model.error.IllegalMoveException;
 
@@ -12,6 +12,24 @@ import java.util.List;
  * This is the model of the entire cabal
  */
 public class Board implements I_BoardModel {
+
+
+
+
+//---------  Genneral methods  -------------------------------------------------------------------------------------
+
+    @Override
+    public boolean isStackComplete(E_PileID pileID) {
+        return false;
+    }
+
+    @Override
+    public List<I_CardModel> getPile(E_PileID pile) {
+        return null;
+    }
+
+
+//---------  Methods for the cardPile and the turnPile  --------------------------------------------------------
 
     @Override
     public I_CardModel turnCard() {
@@ -25,6 +43,8 @@ public class Board implements I_BoardModel {
     }
 
 
+//----------  Move card methods  -----------------------------------------------------------------------------
+
     @Override
     public void move(E_PileID origin, int originPos, E_PileID destination) throws IllegalMoveException {
         //fire property change event of the stacks involved
@@ -32,16 +52,6 @@ public class Board implements I_BoardModel {
 
     @Override
     public boolean canMove(E_PileID origin, int originPos, E_PileID destination) throws IllegalMoveException {
-        return false;
-    }
-
-    @Override
-    public List<I_CardModel> getPile(E_PileID pile) {
-        return null;
-    }
-
-    @Override
-    public boolean isStackComplete(E_PileID pileID) {
         return false;
     }
 
