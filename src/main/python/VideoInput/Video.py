38c4766a73145fccafbfdbeaebba3cac8581e036
grobaxgrappleblast <sile1pepsi1trunte1>
2020-06-11 12:45:53 +0200

    video Size defined as HD

--- src/main/python/VideoInput/Video.py
+++ src/main/python/VideoInput/Video.py
@@ -13,6 +13,8 @@ class Video:
 
         # Video Stats
         self.rec = cv2.VideoCapture(self.VidName,cv2.CAP_DSHOW)
+        self.rec.set(3, 1920)
+        self.rec.set(4, 1080)
 
         # starting the Thread
         self.empt, self.frame = self.rec.read()  # setting the veryFirst Frame just to avoid null pointers.
@@ -38,8 +40,11 @@ class SVideo:
         self.VidName ="VideoInput/video_01.mp4"
         self.FRAME_NAME = " video recording Frame "
 
+
         # Video Stats
         self.rec = cv2.VideoCapture(self.VidName)
+        self.rec.set(3,1920)
+        self.rec.set(4,1080)
 
         # starting the Thread
         self.empt, self.frame = self.rec.read()  # setting the veryFirst Frame just to avoid null pointers.
