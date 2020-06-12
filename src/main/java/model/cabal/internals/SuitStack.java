e07f23c9450580db16a9f6d0fc0eddf68f6b7eea
jacob <jacobriis86@hotmail.com>
2020-06-12 10:05:33 +0200

    Started making SuitStack.java

--- src/main/java/model/cabal/internals/SuitStack.java
+++ src/main/java/model/cabal/internals/SuitStack.java
@@ -4,9 +4,21 @@ import model.cabal.internals.card.I_CardModel;
 import model.error.IllegalMoveException;
 import org.checkerframework.checker.nullness.compatqual.NonNullType;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class SuitStack extends StackBase { //TODO implement this class
+
+
+    public SuitStack(List<I_CardModel> list) {
+        this.stack = list;
+    }
+
+    public SuitStack(){
+        stack = new ArrayList<>();
+    }
+
     @Override
     public Collection<I_CardModel> popSubset(int range) throws IllegalMoveException {
         return null;
