c640f136b0d1451762f9ee92c95022b83a098bee
Claes <cl@inniti.dk>
2020-06-12 20:51:56 +0200

    Added json converter to InputDTO

--- src/main/java/data/InputDTO.java
+++ src/main/java/data/InputDTO.java
@@ -1,5 +1,6 @@
 package data;
 
+import com.google.gson.Gson;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -7,6 +8,8 @@ import io.grpc.ManagedChannelBuilder;
  * This class is intented to translate the input string we get to a json object.
  */
 public class InputDTO {
+    private Gson g = new Gson();
+
     public String getUsrInput(){
         String usrInput ="";
         String target = "localhost:50051";
@@ -21,8 +24,15 @@ public class InputDTO {
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
+        usrInput = stringToJson(usrInput);
         return usrInput;
     }
+
+    public String stringToJson(String toJson){
+        String str = g.toJson(toJson);
+        return str;
+    }
+
 
     //TODO: Transform the string into a JSON object that you use when you initilize the cards
 }
