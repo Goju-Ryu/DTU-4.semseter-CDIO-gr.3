53f49062904b7d4102ea30bc4d6d8a54b73b7785
Goju-Ryu <mads1606@gmail.com>
2020-06-13 10:34:41 +0200

    added inherited docs

--- src/main/java/control/history/GameHistory.java
+++ src/main/java/control/history/GameHistory.java
@@ -3,11 +3,20 @@ package control.history;
 import java.beans.PropertyChangeEvent;
 
 public class GameHistory implements I_GameHistory {
+
+
+    /**
+     * {@inheritDoc}
+     * @param propertyChangeEvent
+     */
     @Override
     public void propertyChange(PropertyChangeEvent propertyChangeEvent) {
 
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean isRepeatState() {
         return false;
