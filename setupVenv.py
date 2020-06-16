6ccde83053cb161ca3022b578a92b59d0d71f405
grobaxgrappleblast <sile1pepsi1trunte1>
2020-06-16 13:47:13 +0200

    ENV script updated

--- setupVenv.py
+++ setupVenv.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 
+
 class myInstaller:
 
     def __init__(self):
@@ -20,41 +21,45 @@ class myInstaller:
             print("python -m pip install --upgrade pip")
             a = subprocess.run("python -m pip install --upgrade pip", capture_output=True)
             if a.returncode == 0:
-                print( a.stdout.decode().split(' ')[0] +" "+a.stdout.decode().split(' ')[1] +" "+ a.stdout.decode().split(' ')[2] )
-            else :
+                print(a.stdout.decode().split(' ')[0] + " " + a.stdout.decode().split(' ')[1] + " " +
+                      a.stdout.decode().split(' ')[2])
+            else:
                 print(a.stderr.decode())
 
             print("\npip install virtualenv")
             a = subprocess.run("pip install virtualenv", capture_output=True)
             if a.returncode == 0:
-                print(a.stdout.decode().split(' ')[0] + " " + a.stdout.decode().split(' ')[1] + " " + a.stdout.decode().split(' ')[2])
+                print(a.stdout.decode().split(' ')[0] + " " + a.stdout.decode().split(' ')[1] + " " +
+                      a.stdout.decode().split(' ')[2])
             else:
                 print(a.stderr.decode())
 
         # CREATING ENVIRONMENT IN FOLDER
         path = str(os.path.dirname(os.path.realpath(__file__)))
-        subprocess.run("python -m venv " + self.environmentName)
+
+        # subprocess.run("python -m venv " + self.environmentName)
         print("\n environment : " + self.environmentName + ". Created as folder in directory")
         print(path)
 
         # Sending Commands to this Environment
         String = "This step of the process will Attmept to \n" \
-                     "install all dependencies from requirements.txt \nin the new environment "\
-                     "Do you want to continue? [y/n]"
+                 "install all dependencies from requirements.txt \nin the new environment " \
+                 "Do you want to continue? [y/n]"
 
         if self.userCheck(String):
             pass
             self.crossProcessCom_()
 
-
     def crossProcessCom_(self):
         dir_path = str(os.path.dirname(os.path.realpath(__file__)))
+        path = str(dir_path + "\\" + self.environmentName + "\\Scripts\\")
 
-        environmentName = "HANS_VENV"
-        path = str(dir_path + "\\" + environmentName + "\\Scripts\\")
-        pipCommand = "pip install -r ..\\..\\requirements.txt"
-        command = "cd " + path + " & activate.bat & " + pipCommand
+        command = "cd " + path + " & activate.bat & " + "pip install -r " + dir_path + "\\requirements.txt"
+        pipUpgrade = "cd " + path + " & activate.bat & " + "python -m pip install --upgrade pip"
 
+        print("\n" + command + "\n")
+
+        subprocess.run(pipUpgrade, shell=True)
         a = subprocess.run(command, capture_output=True, shell=True)
         if a.returncode != 0:
             print(a.stderr)
@@ -72,13 +77,13 @@ class myInstaller:
                 print(str(operation) + str(name))
 
             elif Str[0] == "Requirement":
-                operation ="Already installed \t"
-                name = pStr[1].split(' ')# == "setuptools"
+                operation = "Already installed \t"
+                name = pStr[1].split(' ')  # == "setuptools"
                 print(str(operation) + str(name[1]))
 
             else:
                 pass
-                #print("\n"+r+"\n")
+                # print("\n"+r+"\n")
 
     def userCheck(self, Question):
         accept = False
@@ -87,19 +92,17 @@ class myInstaller:
             try:
                 val = str(input(":"))
             except ValueError:
-                print("sorry i did not understand that input '"+val+"' plaese try again" )
+                print("sorry i did not understand that input '" + val + "' plaese try again")
                 continue
 
-            if val == "y" or val =="Y":
+            if val == "y" or val == "Y":
                 return True
-            elif val =="n" or val =="N":
+            elif val == "n" or val == "N":
                 return False
             else:
-                print("sorry i did not understand that input '"+val+"' plaese try again" )
+                print("sorry i did not understand that input '" + val + "' plaese try again")
 
         return accept
-
-
 
 
 install = myInstaller()
