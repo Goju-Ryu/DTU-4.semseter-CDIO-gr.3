5d545fd61ade4af92869676e5f8bd356d994508c
grobaxgrappleblast <sile1pepsi1trunte1>
2020-06-16 10:44:31 +0200

    Script til generering af setupVenv.py

--- setupVenv.py
+++ setupVenv.py
@@ -1,55 +1,105 @@
 import os
 import subprocess
 
-class textColor:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
 class myInstaller:
 
     def __init__(self):
         self.og = "&&"
         self.command = ""
-        self.environmentName = "HANS_VENV"
+        self.environmentName = "PrivateVenv"
         self.first = True
 
     def run(self):
-        noErr = True
-        while noErr:
-            print(textColor.OKGREEN + "..UPGRADING PIP.. \n "+textColor.ENDC +"installing virtual environment \n ")
-            subprocess.run("python -m pip install --upgrade pip")
-            subprocess.run("pip install virtualenv")
 
-            print(textColor.OKGREEN +"..Creating Environment..")
-            subprocess.run("python -m venv " + self.environmentName)
-            print(textColor.ENDC +"environment : " + self.environmentName)
+        String = "This step of the process will upgrade your pip \n" \
+                 "And install virtual environment if it isent installed\n" \
+                 "Do you want to continue? [y/n]"
 
-
-            a = subprocess.run(".\\" + self.environmentName + "\\Scripts\\activate.bat",capture_output=True, text=True)
-
-
+        # INSTALL PREREQUISITES
+        if self.userCheck(String):
+            print("python -m pip install --upgrade pip")
+            a = subprocess.run("python -m pip install --upgrade pip", capture_output=True)
             if a.returncode == 0:
-                print(textColor.OKGREEN + "Installing Dependencies on Local Environment.. \n"+textColor.ENDC +"in : "+ self.environmentName +"\n - - - -  ")
-                a = subprocess.run("pip install -r requirements.txt", capture_output=True, text= True)
-                if a.returncode == 0:
-                    installs = a.stdout.split('\n')
-                    for req in installs:
-                        reqStr = req.split(" in ")
-                        print( textColor.OKBLUE + reqStr[0])
-                else:
-                    installs = a.stdout.split('\n')
-                    for req in installs:
-                        reqStr = req.split(" in ")
-                        print(textColor.WARNING + reqStr[0])
+                print( a.stdout.decode().split(' ')[0] +" "+a.stdout.decode().split(' ')[1] +" "+ a.stdout.decode().split(' ')[2] )
+            else :
+                print(a.stderr.decode())
 
-            print("done")
-            break
+            print("\npip install virtualenv")
+            a = subprocess.run("pip install virtualenv", capture_output=True)
+            if a.returncode == 0:
+                print(a.stdout.decode().split(' ')[0] + " " + a.stdout.decode().split(' ')[1] + " " + a.stdout.decode().split(' ')[2])
+            else:
+                print(a.stderr.decode())
+
+        # CREATING ENVIRONMENT IN FOLDER
+        path = str(os.path.dirname(os.path.realpath(__file__)))
+        subprocess.run("python -m venv " + self.environmentName)
+        print("\n environment : " + self.environmentName + ". Created as folder in directory")
+        print(path)
+
+        # Sending Commands to this Environment
+        String = "This step of the process will Attmept to \n" \
+                     "install all dependencies from requirements.txt \nin the new environment "\
+                     "Do you want to continue? [y/n]"
+
+        if self.userCheck(String):
+            pass
+            self.crossProcessCom_()
+
+
+    def crossProcessCom_(self):
+        dir_path = str(os.path.dirname(os.path.realpath(__file__)))
+
+        environmentName = "HANS_VENV"
+        path = str(dir_path + "\\" + environmentName + "\\Scripts\\")
+        pipCommand = "pip install -r ..\\..\\requirements.txt"
+        command = "cd " + path + " & activate.bat & " + pipCommand
+
+        a = subprocess.run(command, capture_output=True, shell=True)
+        if a.returncode != 0:
+            print(a.stderr)
+            return False
+
+        print("\nDependencies List")
+        textReturn = a.stdout.decode().split('\n')
+        for r in textReturn:
+            pStr = r.split(':')
+            Str = pStr[0].split(' ')
+
+            if Str[0] == "Collecting":
+                operation = Str[0] + "\t"
+                name = Str[1]
+                print(str(operation) + str(name))
+
+            elif Str[0] == "Requirement":
+                operation ="Already installed \t"
+                name = pStr[1].split(' ')# == "setuptools"
+                print(str(operation) + str(name[1]))
+
+            else:
+                pass
+                #print("\n"+r+"\n")
+
+    def userCheck(self, Question):
+        accept = False
+        print("\n" + Question)
+        while True:
+            try:
+                val = str(input(":"))
+            except ValueError:
+                print("sorry i did not understand that input '"+val+"' plaese try again" )
+                continue
+
+            if val == "y" or val =="Y":
+                return True
+            elif val =="n" or val =="N":
+                return False
+            else:
+                print("sorry i did not understand that input '"+val+"' plaese try again" )
+
+        return accept
+
+
 
 
 install = myInstaller()
