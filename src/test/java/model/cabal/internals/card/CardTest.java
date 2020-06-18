d2fcb5392ac85705cf1b8ea8228a50d5a869405e
Rosie Andersen <rosieandersen1@gmail.com>
2020-06-18 09:18:25 +0200

    New card test

--- src/test/java/model/cabal/internals/card/CardTest.java
+++ src/test/java/model/cabal/internals/card/CardTest.java
@@ -1,40 +1,21 @@
 package model.cabal.internals.card;
 
-import model.cabal.internals.card.Card;
-import model.cabal.internals.card.E_CardSuit;
-import model.cabal.internals.card.I_CardModel;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CardTest {
-    I_CardModel faceUp;
-    I_CardModel faceDown;
 
-    @BeforeEach
-    void beforeEach(){
-        faceUp = new Card(E_CardSuit.DIAMONDS, 5);
-        faceDown = new Card();
+    @Test
+    void reveal() {
     }
 
-    //TODO: implement tests and agree on expected behavior
     @Test
     void getSuit() {
     }
 
     @Test
-    void getSuitText() {
-    }
-
-    @Test
     void getRank() {
-    }
-
-    @Test
-    void getRankValue() {
     }
 
     @Test
@@ -46,15 +27,18 @@ class CardTest {
     }
 
     @Test
+    void equals() {
+    }
+
+    @Test
+    void equals1() {
+    }
+
+    @Test
     void toStringValue() {
     }
 
     @Test
-    void equals() {
-        var c1 = new Card(E_CardSuit.HEARTS, 1);
-        var c2 = new Card(E_CardSuit.HEARTS, 1);
-
-        assertTrue(c1.equals(c2));
-        assertEquals(c1, c2);
+    void rankToString() {
     }
 }
