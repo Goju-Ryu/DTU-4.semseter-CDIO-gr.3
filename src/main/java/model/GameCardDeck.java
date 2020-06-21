162b5ae3cc60a63d1367cacee96b420b5f343512
grobaxgrappleblast <sile1pepsi1trunte1>
2020-06-22 01:00:15 +0200

    GameCardDeck - reImplementered

--- src/main/java/model/GameCardDeck.java
+++ src/main/java/model/GameCardDeck.java
@@ -4,30 +4,32 @@ import model.cabal.internals.card.Card;
 import model.cabal.internals.card.E_CardSuit;
 import model.cabal.internals.card.I_CardModel;
 
+import javax.annotation.Nonnull;
 import java.awt.*;
 import java.util.*;
 import java.util.List;
 import java.util.logging.Logger;
 
-public class GameCardDeck {
+public class GameCardDeck implements Iterable<I_CardModel> {
 
     private Logger log;
 
-    Map<String,I_CardModel> listCardsAll;
+    LinkedHashMap<String,I_CardModel> listCardsAll;
 
     public GameCardDeck() {
-        listCardsAll = new HashMap<>(52);
+        listCardsAll = new LinkedHashMap<>(52);
         for (E_CardSuit suit : E_CardSuit.values()) {
             for (int i = 0; i < 13; i++) {
-                listCardsAll.put(suit.name() +"-"+i + 1 ,new Card(suit, i + 1));
+
+                int key = i + 1;
+                listCardsAll.put(suit.name() +"-"+key ,new Card(suit, key));
             }
         }
-
     }
 
-
-    public boolean remove(I_CardModel o) {
-        I_CardModel a = listCardsAll.remove(o.getSuit().name() +"-"+o.getRank());
+    public boolean remove(I_CardModel o)  {
+        String key = o.getSuit().name() +"-"+o.getRank();
+        I_CardModel a = listCardsAll.remove(key);
         if(a == null)
             return false;
 
@@ -45,4 +47,42 @@ public class GameCardDeck {
         int val2 = o2.getRank() + 13*(o2.getSuit().ordinal());
         return val1 - val2;
     };
+
+
+    public Iterator<I_CardModel> iterator() {
+        Iterator<I_CardModel> it = new Iterator<I_CardModel>() {
+
+            private int currentIndex = 0;
+            List<I_CardModel> list = new ArrayList<>(List.copyOf(listCardsAll.values()));
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < list.size() && list.get(currentIndex) != null;
+            }
+
+            @Override
+            public I_CardModel next() {
+
+                return list.get(currentIndex++);
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+        return it;
+    }
+
+    public int size(){
+        return listCardsAll.size();
+    }
+
+    public boolean removeAll(Collection<I_CardModel> c) {
+        boolean a = false;
+        for (I_CardModel card: c ) {
+            a = remove(card);
+        }
+        return a;
+    }
 }
