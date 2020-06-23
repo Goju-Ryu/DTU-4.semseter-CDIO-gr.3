7f84da7264a2de0e1f962f460c084df3f7adfdd6
Goju-Ryu <mads1606@gmail.com>
2020-06-23 20:13:52 +0200

    Made RefBoard make a random board as default

--- src/main/java/model/cabal/RefBoard.java
+++ src/main/java/model/cabal/RefBoard.java
@@ -1,5 +1,6 @@
 package model.cabal;
 
+import model.GameCardDeck;
 import model.cabal.internals.BuildStack;
 import model.cabal.internals.DrawStack;
 import model.cabal.internals.I_SolitaireStacks;
@@ -8,10 +9,8 @@ import model.cabal.internals.card.Card;
 import model.cabal.internals.card.I_CardModel;
 import model.error.IllegalMoveException;
 
-import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.util.*;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static java.util.AbstractMap.SimpleEntry;
@@ -21,9 +20,8 @@ import static model.cabal.internals.card.E_CardSuit.*;
 public class RefBoard extends AbstractBoardUtility implements I_BoardModel {
 
 
-
     public RefBoard() {
-        this(stdBoard);
+        this(randBoard());
     }
 
     public RefBoard(I_BoardModel boardModel) {
@@ -137,7 +135,54 @@ public class RefBoard extends AbstractBoardUtility implements I_BoardModel {
         return from.canMoveFrom(range);
     }
 
-    private static Map<String, List<I_CardModel>> stdBoard = Map.of(
+    private static Map<String, List<I_CardModel>> randBoard() {
+        var map = new HashMap<String, List<I_CardModel>>();
+        var deck = new GameCardDeck();
+
+        var drawStack = new ArrayList<I_CardModel>();
+        for (int i = 0; i < 24; i++) {
+            drawStack.add(getRandCard(deck));
+        }
+        map.put(DRAWSTACK.name(), drawStack);
+
+        for (int i = 1; i <= 7; i++) {
+            var buildStack = new ArrayList<I_CardModel>();
+            for (int j = 0; j < i; j++) {
+                buildStack.add(getRandCard(deck));
+            }
+            map.put(BUILDSTACK1.name().replace("1", Integer.toString(i)), buildStack);
+        }
+
+
+        return map;
+    }
+
+    /**
+     * @return a random face up card within the confines of legal suit and rank values.
+     * The same value is never drawn twice.
+     */
+    private static I_CardModel getRandCard(GameCardDeck deck) {
+        if (deck.size() == 0)
+            throw new NoSuchElementException();
+
+        var deckIterator = deck.iterator();
+        var rand = new Random();
+        var randIndex = rand.nextInt(deck.size());
+
+        I_CardModel card = deckIterator.next(); //Todo be wary of this, might be an error but i'm not sure
+        for (int i = 0; i < randIndex; i++) {
+            card = deckIterator.next();
+        }
+
+        if (card == null)
+            throw new NoSuchElementException();
+
+        deck.remove(card);
+
+        return card;
+    }
+
+    public static Map<String, List<I_CardModel>> stdBoard = Map.of(
             DRAWSTACK.name(), List.of( //24 cards
                     new Card(HEARTS,2 ),
                     new Card(SPADES, 1),
