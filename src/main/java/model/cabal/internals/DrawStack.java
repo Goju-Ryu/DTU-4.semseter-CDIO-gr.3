bd7962dca7b73f9f4250d3d85c8e06b677e03ea5
grobaxgrappleblast <sile1pepsi1trunte1>
2020-06-25 04:25:09 +0200

    drawstack getsafedrawindex replaced with drawindex

--- src/main/java/model/cabal/internals/DrawStack.java
+++ src/main/java/model/cabal/internals/DrawStack.java
@@ -107,7 +107,7 @@ public class DrawStack extends StackBase implements I_SolitaireStacks  {
     @Override
     public I_CardModel getCard(int position) {
 
-        return stack.get((getSafeDrawIndex() + position + 1) % size());
+        return stack.get((drawIndex + position +1 ) % size());
     }
 
     @Override
